0c183d0b057c0ba2857a980a92fd5772afbdc9f9
PeeHaa <info@pieterhordijk.com>
2013-10-11 13:41:05 +0200

    Added library bootstrap

---
+++ src/Pushy/bootstrap.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This bootstraps the Pushy library
+ *
+ * PHP version 5.4
+ *
+ * @category   Pushy
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    0.0.1
+ */
+namespace Commentar;
+
+use Commentar\Pushy\Autoloader;
+
+/**
+ * Setup the library autoloader
+ */
+require_once __DIR__ . '/Core/Autoloader.php';
+
+$autoloader = new Autoloader(__NAMESPACE__, dirname(__DIR__));
+$autoloader->register();
+
+/**
+ * Setup the password compat lib
+ */
+require __DIR__ . '/Security/password_compat.php';
+
+/**
+ * Setup the doctrine/dbal autoloader
+ */
+require __DIR__ . '/../../vendor/autoload.php';
