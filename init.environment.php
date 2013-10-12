5638b45212821386c69cec34f57ca168122bca2a
PeeHaa <info@pieterhordijk.com>
2013-10-12 18:44:57 +0200

    Added example db connection

--- init.environment.php
+++ init.environment.php
@@ -4,3 +4,10 @@
  * Setup the timezone
  */
 date_default_timezone_set('Europe/Amsterdam');
+
+/**
+ * Setup the database connection
+ */
+$dbConnection = new \PDO('mysql:dbname=pushy;host=127.0.0.1;charset=utf8', 'user', 'pass');
+$dbConnection->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+$dbConnection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
